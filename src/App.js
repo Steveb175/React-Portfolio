1648233afa0fea6e61a561f52c588351fa363437
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-06-24 08:54:50 -0500

    App and Index made

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar.Js";
+import Section from "./components/Section";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Section />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
