39578538fc502c15d21f0dbb3dfd142da7d34200
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-06-25 18:51:18 -0500

    fixed styling bugs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 import "./styles/styles.css";
 
-const App = () => <PortfolioContainer />;
+const App = () => (
+  <div className="custom-body">
+    <PortfolioContainer />
+  </div>
+);
 
 export default App;
