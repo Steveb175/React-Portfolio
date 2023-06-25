68cc5927770b3de1b2bcdca89508cee829923de2
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-06-25 11:22:45 -0500

    Project page done

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function About() {
+export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
-    <div>
+    <div className="container">
       <h1>Contact</h1>
-      <p>There will be a contact form here at some point</p>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="form-control"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            Message:
+          </label>
+          <textarea
+            id="message"
+            className="form-control"
+            value={message}
+            onChange={handleMessageChange}
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,83 @@
 import React from "react";
 
-export default function About() {
+export default function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      <p>Work will display here when coded in</p>
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+        <div className="col">
+          <div className="card">
+            <a href="https://steveb175.github.io/Brewery-Finder/">
+              <img
+                src="/brewery-finder.PNG"
+                alt="Brewery Finder"
+                className="card-img-top"
+                height="300"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <a href="https://steveb175.github.io/Weather-Dashboard/">
+              <img
+                src="/weather-dashboard.PNG"
+                alt="Weather Dashboard"
+                className="card-img-top"
+                height="300"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <a href="https://github.com/Steveb175/BattlehawksTailgate">
+              <img
+                src="/Tailgates.PNG"
+                alt="Battlehawks Tailgates"
+                className="card-img-top"
+                height="300"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <a href="https://github.com/Steveb175/Employee-Tracker">
+              <img
+                src="/employee-tracker.png"
+                alt="Employee Tracker"
+                className="card-img-top"
+                height="300"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <a href="https://github.com/Steveb175/README-Generator">
+              <img
+                src="/readme-generator.png"
+                alt="README Generator"
+                className="card-img-top"
+                height="300"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <a href="https://github.com/Steveb175/Daily-Planner">
+              <img
+                src="/Daily-Planner-Gif.gif"
+                alt="Daily Planner"
+                className="card-img-top"
+                height="300"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
